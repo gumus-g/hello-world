f51893c0a11fe4cac68a73d3b2121b8aa179ccf6
Gulper <gulpergumus@gmail.com>
2021-04-24 13:35:33 -0700

    Added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Gulper Gumus on 4.24.2021
+ */
 public class HelloWorld {
+    public static void main(String[] arg) {
+        System.out.println("Hello, World!");
+    }
 }
